5f5fd1ed67850f19170173e8611a3478f9c9c031
ElianCFG <eliancfg.fajardo@gmail.com>
2024-11-19 21:27:40 -0600

    Para los compas

---
+++ appExamenFinal/Archivostxt.h
@@ -0,0 +1,172 @@
+class Archivostxt{
+private:
+	//Hacer una clase para guardar en el binario
+	string nombreCliente;
+	char nombreProducto[25];
+	int numProducto;
+public:
+	
+	//Setter
+	void setNombreProducto(string nombre){
+		strncpy(nombreProducto, nombre.c_str(), sizeof(nombreProducto) - 1);
+		nombreProducto[sizeof(nombreProducto) - 1] = '\0';
+	}
+	//getter
+	//metodos de calculo
+	void imprimirClientesDelArchivo() {
+		ifstream archClientes("Clientes.txt");  
+		if (archClientes.fail()) {
+			cout << "No se pudo abrir el archivo." <<endl;
+		}
+		int id;
+		string nombre;
+		string tele;
+		string direccion;
+		while (archClientes>> id >>nombre>>tele>>direccion) {
+			cout<<"id: "<<id<<" nombre: "<<nombre <<" telefono: "<<tele<<" Direccion "<<direccion<<endl;
+		}
+		
+		archClientes.close();  
+		}
+			
+	void imprimirProductosDelArchivo() {
+		ifstream archProductos("Productos.txt",ios::in);  
+		if (archProductos.fail()) {
+			cout << "No se pudo abrir el archivo Productos.txt" <<endl;
+		} else {
+			int id;
+			string nombre;
+		
+			while (archProductos>>id>>nombre) {
+					cout <<"id:"<<id<<" nombre: "<<nombre<<endl;
+			}
+		}
+		archProductos.close();  
+	}	
+	
+	void MatricularCliente(){/////METODO DE MATRICULARCLIENTE
+		int idCliente;
+		cout<<"Digite la identificacion del cliente: "<<endl;
+		cin>>idCliente;
+		if(existeCliente(idCliente)==true){
+			int IdProducto;
+			otraVez:
+			cout<<nombreCliente<<" Que producto desea elegir??? "<<endl;
+			cin>>IdProducto;
+			if(existeProducto(IdProducto)==true){
+				cout<<nombreProducto<<" se procedera a guardarse en la base de datos "<<endl;
+				ofstream nuevoDatoCliente(nombreCliente+".txt",ios::binary|ios::app);
+				nuevoDatoCliente.write(reinterpret_cast<char *>(&numProducto), sizeof(numProducto));
+				nuevoDatoCliente.write(reinterpret_cast<char *>(&nombreProducto), sizeof(nombreProducto));
+				nuevoDatoCliente.close();
+				int decision;
+				cout<<nombreCliente<<" desea seguir comprando?"<<endl;
+				cout<<"1.Si"<<endl;
+				cout<<"2.No"<<endl;
+				cin>>decision;
+				switch(decision){
+				case 1:{
+					goto otraVez;
+				}
+				case 2:{
+						cout<<"Chaito,que metodo,parece corto pero es un metodo que llama a otros"<<endl;
+						break;
+					}
+				default:{
+							cout<<"Digite correctamente,sí?"<<endl;
+							break;
+						}
+				}
+			}
+			
+		}else{cout<<"Dicho cliente no existe"<<endl;}
+	 
+	}
+	void verPedidoDeUnCliente(){
+		string nombreArch;
+		cout<<"Digite el nombre del cliente(En mayuscula porfis)"<<endl;
+		cin>>nombreArch;
+		ifstream ClienteLectura(nombreArch+".txt",ios::in); 
+		if (!ClienteLectura) //operador sobre cargado
+		{
+			cout << "Dicha persona no ha realizado pedidos" << endl;
+			system("pause");
+			
+		}else{
+			cout<<"Productos pedidos por "<<nombreArch<<endl;
+			char nombreLProducto[25];
+			ClienteLectura.seekg(0);
+			ClienteLectura.read(reinterpret_cast<char *>(&numProducto), sizeof(numProducto));
+			ClienteLectura.read(reinterpret_cast<char *>(&nombreLProducto), sizeof(nombreLProducto));
+			while (!ClienteLectura.eof()){
+				if (ClienteLectura.good())
+				{
+					cout<<numProducto<<" ";
+					for(int i=0;i<25;i++){
+					cout<<nombreLProducto[i];
+					}
+					cout<<endl;
+					
+				}
+				ClienteLectura.read(reinterpret_cast<char *>(&numProducto), sizeof(numProducto));
+				ClienteLectura.read(reinterpret_cast<char *>(&nombreLProducto), sizeof(nombreLProducto));
+			}
+			
+			ClienteLectura.close();
+			cout << "Productos recuperadas del archivo del binario." << endl;
+		}
+	}	
+	//Metodos de confirmacion-------------------------------------------------
+	bool existeCliente(int idCliente){
+		ifstream archClientes("Clientes.txt");  
+		if (archClientes.fail()) {
+			cout << "No se pudo abrir el archivo." << std::endl;
+			return false;
+		}else{
+		int id;
+		string nombre;
+		string tele;
+		string direccion;
+		while (archClientes>> id >>nombre>>tele>>direccion) {
+			if(idCliente==id){
+				cout<<"id: "<<id<<" nombre: "<<nombre <<" telefono: "<<tele<<" Direccion "<<direccion<<endl;
+				cout<<"Existe este cliente"<<endl;
+				nombreCliente=nombre;
+				return true;
+			}
+		}
+		return false;
+		cout<<"El cliente no existe"<<endl;
+		}
+		archClientes.close(); 
+		return false;
+	}	
+		//Para producto
+	bool existeProducto(int idProducto){
+		ifstream archProductos("Productos.txt",ios::in);
+		if (archProductos.fail()) {
+			cout << "No se pudo abrir el archivo Productos.txt" <<endl;
+		} else {
+			int id;
+			string nombre;
+			while (archProductos>>id>>nombre) {
+				if(id==idProducto){
+				cout <<"id: "<<id<<" nombre: "<<nombre<<endl;
+				cout<<"El producto existe "<<endl;
+				setNombreProducto(nombre);
+				numProducto=id;
+				return true;
+				}
+			}
+			cout<<"El producto no existe"<<endl;
+			return false;
+		}
+		archProductos.close();  
+	}
+		
+			
+			
+			
+			
+	
+};
---
+++ appExamenFinal/main.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+#include <cstring>
+#include <fstream>
+#include <vector>
+#include <sstream>
+using namespace std;
+#include "Cliente.h"
+#include "Archivostxt.h"
+
+Archivostxt arch;
+int main (int argc, char *argv[]) {
+	
+	//RECORDATORIO  si me da tiempo:pon la libreria de lenguaje porque los txt se ven feos
+	int opcion;
+	do{
+		
+	cout<<"1.Ver listado de clientes"<<endl;
+	cout<<"2.Ver listado de productos"<<endl;
+	cout<<"3.Realizar pedido "<<endl;
+	cout<<"4.Ver pedido de un cliente"<<endl;
+	cout<<"5.Salir"<<endl;
+	cin>>opcion;
+	system("cls");
+	switch(opcion){
+		
+	case 1:{
+		
+		arch.imprimirClientesDelArchivo();
+		
+		system("pause");
+		system("cls");
+		break;
+	}
+	case 2:{
+		arch.imprimirProductosDelArchivo();
+		system("pause");
+		system("cls");
+		break;
+	}
+	case 3:{
+		
+		arch.MatricularCliente();
+		system("pause");
+		system("cls");
+		break;
+	}
+	case 4:{
+			arch.verPedidoDeUnCliente();
+			system("pause");
+			system("cls");
+			break;
+		}
+	case 5:{
+			int opcion2;
+			cout<<"Seguro que desea salir?"<<endl;
+			cout<<"1Sí"<<endl;
+			cout<<"2.No"<<endl;
+			cin>>opcion2;
+			switch(opcion2){
+				case 1:{
+				cout<<"Chaito"<<endl;
+				break;
+				}
+				case 2:{
+				opcion=0;
+				cout<<"Usted seguira en el programa "<<endl;
+				system("pause");
+				system("cls");
+				break;
+				}
+			}
+			break;
+		}
+	}
+	
+	} while(opcion!=5);
+	
+	
+
+	system("pause");
+	system("cls");
+	return 0;
+}
+
